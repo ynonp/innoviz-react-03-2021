0976b2a9ffb54ec1cbee805d96d539bb852e1b30
ynonp <ynonperek@gmail.com>
2021-04-18 10:42:44 +0300

    add results url

--- demos/11-forms/src/App.js
+++ demos/11-forms/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const url = 'https://envytetlgk8nivb.m.pipedream.net';
+const resultsUrl = 'https://pipedream.com/@ynonperek/lab1-p_yKCD9o2';
 
 function App() {
   const [submitted, setSubmitted] = useState(false);
